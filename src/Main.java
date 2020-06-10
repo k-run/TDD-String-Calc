c14a4091c1eaec278e1540253e9b898890b6e82f
Karan <karan.chapaneri@gmail.com>
2020-06-10 16:14:36 +0530

    TDD - String calc - allowing new lines in delimiters and negatives

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,64 @@
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.println(Add("4,2,1"));
+
+        //System.out.println(Add("1\n2,3,-6"));
     }
     
     public static int Add(String numbers){
         // if input string is empty, return 0
-
         if (numbers.length() == 0) return 0;
 
         // if input string contains only 1 value
-        if(numbers.split(",").length == 1) {
+        if (numbers.split("\n|,").length == 1) {
              try{
                  // if input string contains only 1 number, return that number
-                 return Integer.parseInt(numbers.split(",")[0]);
+                  int n = Integer.parseInt(numbers.split("\n|,")[0]);
+                  if(n < 0) throw new Exception("Negatives not allowed: " + n);
+                  return n;
              }
              catch (Exception e) {
                  // if input string contains 1 value other than a parsable int,return 0
+                 e.printStackTrace();
                  return 0;
              }
         }
 
         // if input string contains of 2 numbers, add them and return the result
-        if(numbers.split(",").length == 2) {
+        if(numbers.split("\n|,").length == 2) {
             int a = 0, b = 0;
             try {
-                a = Integer.parseInt(numbers.split(",")[0]);
-                b = Integer.parseInt(numbers.split(",")[1]);
+                a = Integer.parseInt(numbers.split("\n|,")[0]);
+                if(a < 0) throw new Exception("Negatives not allowed: " + a);
+
+                b = Integer.parseInt(numbers.split("\n|,")[1]);
+                if(b < 0) throw new Exception("Negatives not allowed: " + b);
                 return a + b;
-            } catch (Exception e) {
+            }
+            catch (NumberFormatException e) {
                 return ((Integer.class.isInstance(a)) ? a : b);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return 0;
             }
         }
 
         // allowing unknown number of numbers for add operation
 
-            String[] str = numbers.split(",");
+            String[] str = numbers.split("\n|,");
             int[] arr = new int[str.length];
 
             int sum = 0;
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
+                if(arr[i] < 0){
+                    try {
+                        throw new Exception("Negatives not allowed: " + arr[i]);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 sum += arr[i];
             }
 
